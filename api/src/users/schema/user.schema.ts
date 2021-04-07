353c0dd0e878e18122fab4d03a3c1649cc454f59
Mak Magz <marktmagcanta.dev@gmail.com>
2021-04-07 19:44:38 +0800

    style: rename schema export to UserSchema

--- api/src/users/schema/user.schema.ts
+++ api/src/users/schema/user.schema.ts
@@ -13,4 +13,4 @@ export class User {
 	@Prop({ required: true })
 	password: string;
 }
-export const CatSchema = SchemaFactory.createForClass(User);
+export const UserSchema = SchemaFactory.createForClass(User);
