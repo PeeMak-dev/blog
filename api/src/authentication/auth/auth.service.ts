03dc05ba6b22bc3389a7b40103188c32bf525d8c
Mak Magz <marktmagcanta.dev@gmail.com>
2021-04-09 04:47:05 +0800

    feat: add user validation in auth service

--- api/src/authentication/auth/auth.service.ts
+++ api/src/authentication/auth/auth.service.ts
@@ -1,26 +1,16 @@
+import { UsersService } from 'src/users/users.service';
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+	constructor(private userService: UsersService) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+	async validateUser(email: string, pass: string) {
+		const user = await this.userService.findUserByEmail(email);
+		console.log('validate: ', user);
+		if (user && user.password === pass) {
+			return user;
+		}
+		return null;
+	}
 }
