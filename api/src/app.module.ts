af3d59d07d10f48fff73eebf6dde5e6ac33229f1
Mak Magz <marktmagcanta.dev@gmail.com>
2021-04-09 02:57:40 +0800

    feat: create auth resources

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,14 +4,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthService } from './authentication/auth/auth.service';
+import { AuthModule } from './authentication/auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.DATABASE_URL),
-    UsersModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		MongooseModule.forRoot(process.env.DATABASE_URL),
+		UsersModule,
+		AuthModule,
+	],
+	controllers: [AppController],
+	providers: [AppService, AuthService],
 })
 export class AppModule {}
