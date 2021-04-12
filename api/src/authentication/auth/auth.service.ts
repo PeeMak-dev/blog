78b9139db17b5b5651955027e843b12a070d9c49
Mak Magz <marktmagcanta.dev@gmail.com>
2021-04-12 17:14:34 +0800

    refactor: add roles property to the payload

--- api/src/authentication/auth/auth.service.ts
+++ api/src/authentication/auth/auth.service.ts
@@ -20,7 +20,11 @@ export class AuthService {
 	}
 
 	async login(user: any) {
-		const payload = { email: user.email, username: user.username };
+		const payload = {
+			email: user.email,
+			username: user.username,
+			roles: user.roles,
+		};
 
 		return {
 			access_token: this.jwtService.sign(payload),
