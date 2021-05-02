c0b125f5bc7e720bb1e49819fa33acf758bc32c6
Mak Magz <marktmagcanta.dev@gmail.com>
2021-05-02 17:29:20 +0800

    feat: add register form template

--- frontend/src/app/pages/register/register.module.ts
+++ frontend/src/app/pages/register/register.module.ts
@@ -1,17 +1,27 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 
-
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-    RegisterComponent
-  ],
+  declarations: [RegisterComponent],
   imports: [
     CommonModule,
-    RegisterRoutingModule
-  ]
+    RegisterRoutingModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class RegisterModule { }
+export class RegisterModule {}
