ad5f220da0c932114a1c61f178aa8f6576ab234d
Mak Magz <marktmagcanta.dev@gmail.com>
2021-05-04 21:21:21 +0800

    feat: add data table

--- frontend/src/app/pages/users/users.component.ts
+++ frontend/src/app/pages/users/users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
+import { User } from 'src/app/shared/models/interfaces/users/user.interface';
 import { Users } from 'src/app/shared/models/interfaces/users/users.interface';
 import { UsersService } from 'src/app/shared/services/users/users.service';
 
@@ -9,6 +11,9 @@ import { UsersService } from 'src/app/shared/services/users/users.service';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = ['name', 'username', 'email'];
+  dataSource: MatTableDataSource<User>;
+
   private userDataSubscription: Subscription;
 
   constructor(private usersService: UsersService) {}
@@ -18,7 +23,12 @@ export class UsersComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.userDataSubscription = this.usersService._allUsers.subscribe(
-      (data: Users) => console.log(data)
+      (data: Users) => {
+        console.log(data);
+        if (data) {
+          this.dataSource = new MatTableDataSource(data.docs);
+        }
+      }
     );
   }
 }
--- frontend/src/app/pages/users/users.module.ts
+++ frontend/src/app/pages/users/users.module.ts
@@ -1,17 +1,18 @@
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 
-
 @NgModule({
-  declarations: [
-    UsersComponent
-  ],
+  declarations: [UsersComponent],
   imports: [
     CommonModule,
-    UsersRoutingModule
-  ]
+    UsersRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
 })
-export class UsersModule { }
+export class UsersModule {}
